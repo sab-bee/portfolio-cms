01330c86c2746e4d6169b9a392a17bbfbc44b882
Sabbir <61304556+sabbir-d@users.noreply.github.com>
2023-08-03 03:27:45 +0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        introduction: "storyblok/Introduction",
+        about: "storyblok/About",
+        experience: "storyblok/Experience",
+        singleExperience: "storyblok/SingleExperience",
+        project: "storyblok/Project",
+        singleProject: "storyblok/SingleProject"
+      }
+    }),
+    tailwind({
+      applyBaseStyles: true
+    })
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  }
+});
