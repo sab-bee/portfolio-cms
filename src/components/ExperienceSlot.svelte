3378e3b6c445726326c4e0f4804b88eea72f7c50
Sabbir <61304556+sabbir-d@users.noreply.github.com>
2023-08-19 15:38:01 +0600

    fix rotate

--- src/components/ExperienceSlot.svelte
+++ src/components/ExperienceSlot.svelte
@@ -20,22 +20,25 @@
         });
         toggles.forEach((toggle) => {
           toggle.classList.remove("rotate");
+          toggle.classList.add("rotateBack");
         });
         if (reference !== i) {
           descs[i].classList.remove("collapse");
           reference = i;
           toggles[i].classList.add("rotate");
+          toggles[i].classList.remove("rotateBack");
         } else {
           descs[i].classList.add("collapse");
           reference = -1;
           toggles[i].classList.remove("rotate");
+          toggles[i].classList.add("rotateBack");
         }
       });
     });
     })
 </script>
 
-<div class="border-b border-black last:border-none " class:shadow={css}>
+<div>
   <div
     class="tab select-none py-9 grid grid-cols-4 items-center cursor-pointer hover:bg-shade transition-colors px-8"
   >
@@ -44,7 +47,7 @@
       <h3 class="text-lg m-0 font-normal">{blok.title}</h3>
       <p class="m-0">{blok.duration}</p>
     </div>
-    <button class:rotate={css} class="toggle justify-self-end  w-10 h-10 rounded-full border border-black text-2xl col-span-1">+</button>
+    <button class:rotate={css} class:rotateBack={css} class="toggle justify-self-end  w-10 h-10 rounded-full border border-black text-3xl col-span-1 text-neutral-700">+</button>
   </div>
   <div class="desc mt-2 collapse mb-8 px-8">
     {@html renderTask}
@@ -77,15 +80,25 @@
     animation: rotation 0.2s forwards;
   }
 
+  .rotateBack {
+    animation: rotationBack 0.2s forwards;
+  }
+
   @keyframes rotation {
     from {
       transform: rotate(0);
     }
     to {
       transform: rotate(45deg);
-      background: transparent;
-      border: 1px solid black;
-      color: black;
+    }
+  }
+
+  @keyframes rotationBack {
+    from {
+      transform: rotate(45deg);
+    }
+    to {
+      transform: rotate(0);
     }
   }
 </style>
