29a84139dc224b8fc0c17e109efb551a06c06fcd
Sabbir <61304556+sabbir-d@users.noreply.github.com>
2023-08-22 19:30:42 +0600

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,34 +4,35 @@ import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import netlify from "@astrojs/netlify/functions";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: "storyblok/Page",
-        introduction: "storyblok/Introduction",
-        about: "storyblok/About",
-        experience: "storyblok/Experience",
-        workExperience: "storyblok/SingleExperience",
-        project: "storyblok/Project",
-        singleProject: "storyblok/SingleProject",
-        testimonial: "storyblok/Review",
-        review: "storyblok/SingleReview",
-        contact: "storyblok/Contact"
-      }
-    }),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    svelte()
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    bridge: true,
+    components: {
+      page: "storyblok/Page",
+      introduction: "storyblok/Introduction",
+      about: "storyblok/About",
+      experience: "storyblok/Experience",
+      workExperience: "storyblok/SingleExperience",
+      project: "storyblok/Project",
+      singleProject: "storyblok/SingleProject",
+      testimonial: "storyblok/Review",
+      review: "storyblok/SingleReview",
+      contact: "storyblok/Contact"
+    }
+  }), tailwind({
+    applyBaseStyles: false
+  }), svelte()],
   vite: {
     plugins: [basicSsl()],
     server: {
       https: true
     }
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
